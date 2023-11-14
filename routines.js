a89901e0833d49797b44c400232b645a7ec7760b
rushik0507 <rushikpateljnv@gmail.com>
2023-11-14 23:05:21 +0530

    routines

---
+++ routines.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  var $list = $("#menu");
+  var $ddl = $("#ddl");
+  $list.click(function () {
+    $ddl.toggleClass("active");
+  });
+});
+$(document).ready(function () {
+  var $list = $("#menu1");
+  var $ddl = $("#ddl1");
+  $list.click(function () {
+    $ddl.toggleClass("active");
+  });
+});
+$(document).ready(function () {
+  var $list = $("#menu2");
+  var $ddl = $("#ddl2");
+  $list.click(function () {
+    $ddl.toggleClass("active");
+  });
+});
+$(document).ready(function () {
+  var $list = $("#menu3");
+  var $ddl = $("#ddl3");
+  $list.click(function () {
+    $ddl.toggleClass("active");
+  });
+});
